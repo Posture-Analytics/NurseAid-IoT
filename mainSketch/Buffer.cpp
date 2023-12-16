55edc4f4f309ddd993c5d64ae246a988249dea83
AlmirFonseca <almirfonseca60@gmail.com>
2023-12-16 19:53:32 -0300

    Add a buffer structure for the temperature frame samples

---
+++ mainSketch/Buffer.cpp
@@ -0,0 +1,159 @@
+#include "Buffer.h"
+#include "Errors.h"
+#include "Debug.h"
+
+bool TemperatureDataBuffer::isBufferEmpty() const {
+    return bufferSize == 0;
+}
+
+bool TemperatureDataBuffer::isBufferFull() const {
+    return bufferSize >= BUFFER_CAPACITY;
+}
+
+int TemperatureDataBuffer::getBufferCapacity() const {
+    return BUFFER_CAPACITY;
+}
+
+int TemperatureDataBuffer::getBufferSize() const {
+    return bufferSize;
+}
+
+int TemperatureDataBuffer::getReadIndex() const {
+    return readIndex;
+}
+
+int TemperatureDataBuffer::getWriteIndex() const {
+    return writeIndex;
+}
+
+void TemperatureDataBuffer::moveReadIndexForward() {
+    readIndex = (readIndex + 1) % BUFFER_CAPACITY;
+    bufferSize--;
+}
+
+void TemperatureDataBuffer::moveReadIndexBackward() {
+    readIndex = (readIndex - 1 + BUFFER_CAPACITY) % BUFFER_CAPACITY;
+    bufferSize++;
+}
+
+void TemperatureDataBuffer::moveWriteIndexForward() {
+    writeIndex = (writeIndex + 1) % BUFFER_CAPACITY;
+    bufferSize++;
+}
+
+void TemperatureDataBuffer::moveWriteIndexBackward() {
+    writeIndex = (writeIndex - 1 + BUFFER_CAPACITY) % BUFFER_CAPACITY;
+    bufferSize--;
+}
+
+time_t TemperatureDataBuffer::getCurrentSampleSeconds() const {
+    return buffer[readIndex].timestampMillis / 1000ULL;
+}
+
+void TemperatureDataBuffer::computeCurrentSampleDate(char* sampleDate) {
+    // Get the next sample from the buffer
+    time_t sampleTimestampSec = getCurrentSampleSeconds();
+    // Get the time information from the time_t object
+    localtime_r(&sampleTimestampSec, &timeInfo);
+    // Convert the struct tm to a formatted string like "YYYY-MM-DD\0"
+    strftime(sampleDate, 11, "%F", &timeInfo);
+}
+
+void TemperatureDataBuffer::computeNextDaySeconds() {
+    time_t sampleTimestampSec = getCurrentSampleSeconds();
+    // Get the time info of the next day
+    sampleTimestampSec += 24 * 60 * 60;
+    localtime_r(&sampleTimestampSec, &timeInfo);
+    // Set the time info to the start of the day
+    timeInfo.tm_hour = 0;
+    timeInfo.tm_min = 0;
+    timeInfo.tm_sec = 0;
+
+    // Convert back to time_t and save it
+    nextDay = mktime(&timeInfo);
+}
+
+bool TemperatureDataBuffer::hasDateChanged() {
+    // Compare the current timestamp with the timestamp of the next day
+    return getCurrentSampleSeconds() >= nextDay;
+}
+
+bool TemperatureDataBuffer::isSampleNull(const temperatureData* sample) const {
+    // Check if we have some non null data in the sample struct
+    for (int i = 0; i < FRAME_SIZE; i++) {
+        if (sample->temperaturePixels[i] != 0) {
+            return false;
+        }
+    }
+
+    // Return true if all the sample data is null
+    return true;
+}
+
+const temperatureData* TemperatureDataBuffer::getSample() {
+    // If the buffer is empty
+    if (isBufferEmpty()) {
+        // Return nullptr if the sample was not retrieved from the buffer
+        return nullptr;
+    }
+
+    // Get the next sample from the buffer
+    const temperatureData* ptrSample = &buffer[readIndex];
+
+    // Move the read index to the next sample
+    moveReadIndexForward();
+
+    // Return the sample if it was retrieved from the buffer
+    return ptrSample;
+}
+
+temperatureData* TemperatureDataBuffer::getNewSample() {
+    // If the buffer is full
+    if (isBufferFull()) {
+        // Return nullptr if the sample was not added to the buffer
+        return nullptr;
+    }
+
+    // Get the pointer to the next sample to be written
+    temperatureData* ptrSample = &buffer[writeIndex];
+
+    // Move the write index to the next sample
+    moveWriteIndexForward();
+
+    // Return the pointer to the next sample to be written
+    return ptrSample;
+}
+
+void TemperatureDataBuffer::printBufferState() const {
+    // If the buffer gets full
+    if (isBufferFull()) {
+        // Print an error message and restart the device
+        LogFatalln("Buffer full");
+    }
+
+    // Prints the buffer state
+    LogVerboseln("Buffer state: ", bufferSize, "/", BUFFER_CAPACITY);
+}
+
+void TemperatureDataBuffer::printBufferIndexes() const {
+    LogVerboseln("Buffer index: R=", readIndex, " W=", writeIndex);
+}
+
+void TemperatureDataBuffer::dumpBufferContent(int start, int end) const {
+    // Iterate over the buffer content on the desired range
+    for (int i = start; i < end; i++) {
+        // Get the current sample
+        const temperatureData* sample = &buffer[i];
+
+        // Print the sample timestamp
+        LogDebug(sample->timestampMillis, " ");
+
+        // Print the sample temperature sensor values
+        for (int j = 0; j < FRAME_SIZE; j++) {
+            LogDebug(sample->temperaturePixels[j], " ");
+        }
+
+        // Print the end of the line
+        LogDebugln("\n");
+    }
+}
---
+++ mainSketch/Buffer.h
@@ -0,0 +1,204 @@
+/*
+    Buffer.h
+
+    * This module handles the buffer that stores the data collected from the thermal camera.
+    * The buffer consists of an array of structs and some indexes to keep track of
+    the buffer state. It also provides functions to add and get samples from the buffer,
+    handle buffer capacity and indexes.
+    * For debug purposes, it also provides functions to print the buffer state and dump
+    its content.
+*/
+
+#ifndef Buffer_H_
+#define Buffer_H_
+
+#include <time.h>
+#include <Arduino.h>
+
+#define FRAME_SIZE 768 // 32 * 24
+
+// Define the capacity of the buffer
+const int BUFFER_CAPACITY = 10;
+
+/**
+ * Struct to organize the collected data
+ * 
+ * timestampMillis: timestamp of the sample in milliseconds
+ * temperaturePixels: array of temperatures of the pixels of the thermal camera
+ */
+struct temperatureData {
+    // 8 bytes
+    unsigned long long timestampMillis = 0;
+
+    // 4 bytes * 768 = 3072 bytes per sample
+    float temperaturePixels[FRAME_SIZE] = {0};
+};
+
+// Define a class to store the collected data
+
+/**
+ * Class that handles the buffer that stores the data collected from the thermal camera.
+ * The buffer consists of an array of temperatureData structs and some indexes to keep track of
+ * the buffer state. It also provides functions to add and get samples from the buffer,
+ * handle buffer capacity and indexes.
+ * For debug purposes, it also provides functions to print the buffer state and dump
+ * its content.
+ * 
+ * @param buffer the array of temperatureData structs that stores the collected data
+ * @param bufferSize the number of samples in the buffer
+ * @param readIndex the index of the next sample to be read
+ * @param writeIndex the index of the next sample to be written
+*/
+class TemperatureDataBuffer {
+
+    // Sotre information regarding the time of the read samples
+    struct tm timeInfo;
+
+    // Store the timestamp of the following day to check if the date has changed
+    // The initial value is 0 so that it gets updated during the first use
+    time_t nextDay = 0;
+
+public:
+
+    // Create a buffer based on the temperatureData struct
+    temperatureData buffer[BUFFER_CAPACITY];
+
+    // Hold the number of samples in the buffer
+    int bufferSize = 0;
+
+    // Index to read the next sample
+    int readIndex = 0;
+    // Index to write the next sample
+    int writeIndex = 0;
+
+    /**
+     * Check if the buffer is empty
+     * 
+     * @return true if the buffer is empty, false otherwise
+     */
+    bool isBufferEmpty() const;
+
+    /**
+     * Check if the buffer is full
+     * 
+     * @return true if the buffer is full, false otherwise
+     */
+    bool isBufferFull() const;
+
+    // Get the capacity of the buffer
+    /**
+     * Get the capacity of the buffer
+     * 
+     * @return the capacity of the buffer
+     */
+    int getBufferCapacity() const;
+
+    /**
+     * Get the number of samples in the buffer
+     * 
+     * @return the number of samples in the buffer
+     */
+    int getBufferSize() const;
+
+    /**
+     * Get the index of the next sample to be read
+     * 
+     * @return the index of the next sample to be read
+     */
+    int getReadIndex() const;
+
+    /**
+     * Get the index of the next sample to be written
+     * 
+     * @return the index of the next sample to be written
+     */
+    int getWriteIndex() const;
+
+    /** 
+     * Move the read index to the next sample 
+     */
+    void moveReadIndexForward();
+
+    /** 
+     * Move the read index to the previous sample 
+     */
+    void moveReadIndexBackward();
+
+    /** 
+     * Move the write index to the next sample 
+     */
+    void moveWriteIndexForward();
+
+    /** 
+     * Move the write index to the previous sample in the buffer 
+     */
+    void moveWriteIndexBackward();
+
+    /**
+     * Get the timestamp of the next sample to be read from the buffer
+     * 
+     * @return the timestamp of the next sample to be read from the buffer
+     */
+    time_t getCurrentSampleSeconds() const;
+
+    /**
+     * Get the current sample date. Should receive an array of at least 11 chars
+     * 
+     * @param sampleDate the array of chars to store the sample date
+     */
+    void computeCurrentSampleDate(char* sampleDate);
+
+    /**
+     * Compute the timestamp of the start of the following day
+     */
+    void computeNextDaySeconds();
+
+    /**
+     * Check if the date has changed
+     * 
+     * @return true if the date has changed, false otherwise
+     */
+    bool hasDateChanged();
+
+    /**
+     * Check if the content of the sample is null
+     * 
+     * @param sample the sample to check
+     * @return true if the sample is null, false otherwise
+     */
+    bool isSampleNull(const sensorData* sample) const;
+
+    /**
+     * Get the next sample from the buffer at the read index and increment it
+     * 
+     * @return a pointer to the next sample to be read
+     */
+    const sensorData* getSample();
+
+    /**
+     * Get the next sample from the buffer at the write index and increment it
+     * 
+     * @return a pointer to the next sample to be written
+     */
+    sensorData* getNewSample();
+
+    /**
+     * Print the buffer state, the number of samples in the buffer and its capacity
+     */
+    void printBufferState() const;
+
+    /**
+     * Print the indexes of the buffer
+     */
+    void printBufferIndexes() const;
+
+    /**
+     * Dump the buffer content or a part of it
+     * 
+     * @param start the index of the first sample to dump
+     * @param end the index of the last sample to dump
+     */
+    void dumpBufferContent(int start = 0, int end = BUFFER_CAPACITY) const;
+};
+
+#endif  // Buffer_H_
